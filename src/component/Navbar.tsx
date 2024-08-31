e8c51aaefeba0453629308efee660eefc75b3c04
vedant <vedant@ssipmt.com>
2024-08-31 08:46:54 +0530

    nav responsive

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -2,14 +2,18 @@
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { CiLocationOn, CiSearch } from "react-icons/ci";
 import { FaCheckCircle } from "react-icons/fa";
+import { IoMdMenu } from "react-icons/io";
 
 function Navbar() {
     return (
         <div className="w-screen h-fit flex flex-col ">
             <div className="bg-white w-full">
-                <div className="w-full h-20 flex  justify-around  items-center  border-b-2 border-gray-200">
-                    <div className="w-fit ml-4 flex items-center justify-around">
-                        <div><img src="/logo.png" className='w-40' alt="logo" /></div>
+                <div className="w-full h-20 flex  justify-between items-center  border-b-2 border-gray-200">
+                    <div className="w-fit px-5 lg:ml-5 flex items-center gap-0">
+                        <div className="lg:hidden w-12">
+                            <IoMdMenu />
+                        </div>
+                        <div><img src="/logo.png" className='w-32 md:w-40' alt="logo" /></div>
                     </div>
                     <div className="hidden lg:block w-[70%] ">
                         <div className="w-full flex justify-between">
@@ -25,20 +29,23 @@ function Navbar() {
                             </div>
                         </div>
                     </div>
-                    <div className=' flex items-center justify-around'>
-                        <div className='border border-gray-400 p-1 text-xs text-gray-400 hover:border-blue-600 hover:text-blue-500 rounded-sm'> Login/Signup</div>
+                    <div className='mr-4 md:mr-8 flex items-center justify-around'>
+                        <div className='lg:hidden border  p-1 text-xs border-blue-500 text-blue-500 rounded-sm'> signin</div>
+                        <div className='hidden lg:block border border-gray-400 p-1 text-xs text-gray-400 hover:border-blue-600 hover:text-blue-500 rounded-sm'> Login/Signup</div>
                     </div>
                 </div>
-                <div className="w-full h-24  flex justify-center ">
-                    <div className="mt-4 flex w-[50%]">
-                        <div className=" border border-gray-400 h-10 outline-none px-2 flex w-[40%]  items-center ">
+                <div className="w-full h-16 md:h-24  flex justify-center ">
+                    <div className="mt-4 flex w-full md:w-[75%] lg:w-[50%] justify-center">
+                        <div className=" border border-gray-400 h-10 outline-none px-2 flex w-[80%]  md:w-[50%] lg:w-[40%]  items-center rounded-sm md:rounded-none">
                             <CiLocationOn /> <input type="text" className="px-2 outline-none" name="city" placeholder="Banglore" id="" />
                         </div>
-                        <div className=" border border-gray-400 h-10 outline-none px-2 flex w-[60%]  items-center ">
-                            <CiSearch /> <input type="text" className="px-2 outline-none text-sm mr-3" name="city" placeholder="search doctor,clinic,hospital,etc" id="" />
+                        <div className="hidden md:block border border-gray-400 h-10 outline-none px-2  lg:w-[60%]  items-center ">
+                            <div className="w-full h-full flex items-center">
+                                <CiSearch /> <input type="text" className="px-2 outline-none text-sm mr-3 " name="city" placeholder="search doctor,clinic,hospital,etc" id="" />
+                            </div>
                         </div>
                     </div>
-                    <div className="ml-2 mt-3">
+                    <div className="ml-2 mt-3 hidden lg:block">
                         <p className="text-[10px] text-gray-400">Feed up end less wait?</p>
                         <p className="text-[12px] mt-3 flex items-center">Look for clinic with <p className="text-purple-900 px-1"> Prime </p> <FaCheckCircle className="text-purple-900" /> </p>
                     </div>
--- src/component/StickyNav.tsx
+++ src/component/StickyNav.tsx
@@ -3,13 +3,15 @@ import { RiArrowDropDownLine } from 'react-icons/ri'
 
 function StickyNav() {
     return (
-        <div className="bg-blue-800 w-full h-12 flex justify-center items-center sticky top-0 ">
-            <div className='mx-2 text-xm md:w-[12%] lg:w-[8%] py-[2px] text-white bg-white bg-opacity-20 px-2 rounded-sm flex justify-between items-center'>Gender <RiArrowDropDownLine className="text-xl" /> </div>
-            <div className='mx-2 text-xm md:w-[16%] lg:w-[10%] py-[2px] text-white bg-white bg-opacity-20 px-2 rounded-sm flex justify-between items-center'>Patient Stories <RiArrowDropDownLine className="text-xl" /> </div>
-            <div className='mx-2 text-xm md:w-[14%] lg:w-[8%] py-[2px] text-white bg-white bg-opacity-20 px-2 rounded-sm flex justify-between items-center'>Experience <RiArrowDropDownLine className="text-xl" /> </div>
-            <div className='mx-2 text-xm md:w-[14%] lg:w-[8%] py-[2px] text-white  bg-opacity-20 px-2 rounded-sm flex justify-between items-center'>All Fillter<RiArrowDropDownLine className="text-xl" /> </div>
-            <p className="text-white text-xs inline-flex">Sort By</p>
-            <div className='mx-2 text-xm md:w-[14%] lg:w-[8%] py-[2px] text-white bg-white bg-opacity-20 px-2 rounded-sm flex justify-between items-center'> Relevence<RiArrowDropDownLine className="text-xl" /> </div>
+        <div className='w-full hidden md:block'>
+            <div className="bg-blue-800 w-full h-12 flex justify-center items-center sticky top-0 ">
+                <div className='mx-2 text-xm md:w-[12%] lg:w-[8%] py-[2px] text-white bg-white bg-opacity-20 px-2 rounded-sm flex justify-between items-center'>Gender <RiArrowDropDownLine className="text-xl" /> </div>
+                <div className='mx-2 text-xm md:w-[16%] lg:w-[10%] py-[2px] text-white bg-white bg-opacity-20 px-2 rounded-sm flex justify-between items-center'>Patient Stories <RiArrowDropDownLine className="text-xl" /> </div>
+                <div className='mx-2 text-xm md:w-[14%] lg:w-[8%] py-[2px] text-white bg-white bg-opacity-20 px-2 rounded-sm flex justify-between items-center'>Experience <RiArrowDropDownLine className="text-xl" /> </div>
+                <div className='mx-2 text-xm md:w-[14%] lg:w-[8%] py-[2px] text-white  bg-opacity-20 px-2 rounded-sm flex justify-between items-center'>All Fillter<RiArrowDropDownLine className="text-xl" /> </div>
+                <p className="text-white text-xs inline-flex">Sort By</p>
+                <div className='mx-2 text-xm md:w-[14%] lg:w-[8%] py-[2px] text-white bg-white bg-opacity-20 px-2 rounded-sm flex justify-between items-center'> Relevence<RiArrowDropDownLine className="text-xl" /> </div>
+            </div>
         </div>
     )
 }
